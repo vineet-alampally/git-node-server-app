b3a21b6bb70a31c611d1b1ca976b57b2b389521b
vineet-alampally <48998765+vineet-alampally@users.noreply.github.com>
2022-11-20 19:31:51 -0500

    A8 changes

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -15,9 +15,19 @@ const findTuits = (req, res) =>
 
 const createTuit = (req, res) => {
     const newTuit = req.body;
-    newTuit._id = (new Date()).getTime()+'';
+    newTuit._id = (new Date()).getTime() + '';
     newTuit.likes = 0;
     newTuit.liked = false;
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
+    newTuit.topic = "Node JS Assignment"
+    newTuit.username = "Vineet Alampally";
+    newTuit.handle = "@vineetalampally";
+    newTuit.title = "Node JS";
+    newTuit.time = "Just now";
+    newTuit.image =
+        "https://images.g2crowd.com/uploads/product/image/"
+        + "large_detail/large_detail_f0b606abb6d19089febc9faeeba5bc05/nodejs-development-services.png";
     tuits.push(newTuit);
     res.json(newTuit);
 }
